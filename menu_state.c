0e21c2644c1458d871a01567efcd9fa27599bedf
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2016-04-04 19:58:28 -0300

    I am a HUGE moron

---
+++ menu_state.c
@@ -0,0 +1,61 @@
+// Copyright © 2016 Victor Hermann "vitorhnn" Chiletto
+// Licensed under the MIT/Expat license.
+
+#include <SDL2/SDL_ttf.h>
+
+#include "3rdparty/physicsfs/extras/physfsrwops.h"
+
+#include "main.h"
+#include "texloader.h"
+#include "menu_state.h"
+
+#define MAKERECT(label) {label.x, label.y, label.w, label.h}
+
+typedef struct {
+    SDL_Texture* tex;
+    int x;
+    int y;
+    int w;
+    int h;
+} label;
+
+static TTF_Font*    font            = NULL; 
+static SDL_Texture* bg              = NULL;
+static label        label_play      = {NULL, 500, 100, 0, 0};
+static label        label_config    = {NULL, 500, 200, 0, 0};
+
+void menu_init(SDL_Renderer* renderer) {
+    bg = tex_load_from_file(renderer, "menubg.png");
+    SDL_RWops* ops = PHYSFSRWOPS_openRead("opensans.ttf");
+    font = TTF_OpenFontRW(ops, 1, 50);
+
+    SDL_Color color = {0, 255, 0, SDL_ALPHA_OPAQUE};
+    label_play.tex      = tex_from_text(renderer, "dá pray", font, color, &label_play.w, &label_play.h);
+    label_config.tex    = tex_from_text(renderer, "configura aê", font, color, &label_config.w, &label_config.h);
+}
+
+void menu_handle(SDL_Event* event) {
+    if (event->type == SDL_KEYDOWN) {
+        engine_quit();
+    }
+}
+
+state menu_think(void) {
+    return STATE_NOCHANGE;
+}
+
+void menu_paint(SDL_Renderer* renderer, unsigned diff) {
+#pragma unused (diff)
+    SDL_Rect r = MAKERECT(label_play);
+    SDL_Rect r2 = MAKERECT(label_config);
+
+    SDL_RenderCopy(renderer, bg, NULL, NULL);
+    SDL_RenderCopy(renderer, label_play.tex, NULL, &r);
+    SDL_RenderCopy(renderer, label_config.tex, NULL, &r2);
+}
+
+void menu_quit(void) {
+    TTF_CloseFont(font);
+    SDL_DestroyTexture(label_play.tex);
+    SDL_DestroyTexture(label_config.tex);
+}
---
+++ menu_state.h
@@ -0,0 +1,22 @@
+// Copyright © 2016 Victor Hermann "vitorhnn" Chiletto
+// Licensed undher the MIT/Expat license.
+
+#ifndef MENU_STATE_H
+#define MENU_STATE_H
+
+#include <SDL2/SDL.h>
+#include "common.h"
+
+void menu_init(SDL_Renderer* renderer);
+
+void menu_handle(SDL_Event* event);
+
+state menu_think(void);
+
+void menu_paint(SDL_Renderer* renderer, unsigned diff);
+
+void menu_quit(void);
+
+#endif
+
+// vim: set ts=4 sw=4 expandtab
