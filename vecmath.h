a76559e1aec519c1255346d887a98730e5d3e1b3
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2020-05-27 22:53:47 -0300

    move math code to the header
    makes inlining these functions possible outside of lto

--- vecmath.h
+++ vecmath.h
@@ -4,35 +4,143 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <math.h>
 #include <stdbool.h>
 
 typedef struct {
-    float x, y;
+    float x, y, pad1, pad2;
 } vec2;
 
 typedef struct {
     float x, y, w, h;
 } rect;
 
-vec2 MAKEVEC(float x, float y);
+static vec2 MAKEVEC(float x, float y)
+{
+    vec2 r = {x, y};
 
-float pointdistance(vec2 a, vec2 b);
+    return r;
+}
 
-float pointangle(vec2 a, vec2 b);
+// not really vector math, but I need this, so
+static float pointdistance(vec2 a, vec2 b)
+{
+    float x = powf(b.x - a.x, 2);
+    float y = powf(b.y - a.y, 2);
 
-bool edgecollide(rect a, rect b);
+    return sqrtf(x + y);
+}
 
-bool fullcollide(rect a, rect b);
+static float pointangle(vec2 a, vec2 b)
+{
+    float deltaX = a.x - b.x;
+    float deltaY = a.y - b.y;
 
-vec2 rot_vec(vec2 vec, float angle);
+    return atan2f(deltaY, deltaX);
+}
 
-vec2 get_vec(vec2 a, vec2 b);
+static bool edgecollide(rect a, rect b)
+{
+    // totally not copy and pasted from my old pong clone
+    if (a.y + a.h <= b.y) {
+        return false;
+    }
 
-float norm(vec2 vec);
+    if (a.y >= b.y + b.h) {
+        return false;
+    }
 
-vec2 unit(vec2 vec);
+    if (a.x + a.w <= b.x) {
+        return false;
+    }
 
-vec2 sum(vec2 a, vec2 b);
+    if (a.x >= b.x + b.w) {
+        return false;
+    }
 
-vec2 mul(vec2 vec, float scalar);
+    return true;
+}
+
+static bool fullcollide(rect a, rect b)
+{
+    if (a.y + a.h < b.y) {
+        return false;
+    }
+
+    if (a.y > b.y + b.h) {
+        return false;
+    }
+
+    if (a.x + a.w < b.x) {
+        return false;
+    }
+
+    if (a.x > b.x + b.w) {
+        return false;
+    }
+
+    return true;
+}
+
+static vec2 rot_vec(vec2 vec, float angle)
+{
+    vec2 retvec = {
+        .x = vec.x * cosf(angle) - vec.y * sinf(angle),
+        .y = vec.x * sinf(angle) + vec.y * cosf(angle)
+    };
+
+    return retvec;
+}
+
+static float norm(vec2 vec)
+{
+    return sqrtf(vec.x * vec.x + vec.y * vec.y);
+}
+
+static vec2 unit(vec2 vec)
+{
+    float n = norm(vec);
+
+    if (n == 0) {
+        return vec;
+    }
+
+    vec2 retvec = {
+        .x = vec.x / n,
+        .y = vec.y / n
+    };
+
+    return retvec;
+}
+
+static vec2 get_vec(vec2 a, vec2 b)
+{
+    vec2 retvec;
+    retvec.x = b.x - a.x;
+    retvec.y = b.y - a.y;
+
+    return unit(retvec);
+}
+
+
+static vec2 sum(vec2 a, vec2 b)
+{
+    vec2 retvec = {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+
+    return retvec;
+}
+
+static vec2 mul(vec2 vec, float scalar)
+{
+    vec2 retvec = {
+        .x = vec.x * scalar,
+        .y = vec.y * scalar
+    };
+
+    return retvec;
+}
+
 #endif
